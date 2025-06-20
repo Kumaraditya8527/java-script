f1f363bcab8359aefd64f0131f0c1d4918ec6059
Kumaraditya8527 <adityakumar1886@gmail.com>
2025-06-20 13:13:37 +0000

    corversion & operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let score="33"
+
+// console.log(typeof(score))
+
+// let value=Number(score)
+// console.log(typeof(score))
+
+/*
+"33"==>33
+"33abc"==>NaN
+"true"==>1 and false==>0
+*/
+
+/*
+1==>true and 0==>false
+""==>false and "aditya"==>true
+*/
+
+//*************** operations***************
+
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+let str1="hello"
+let str2="aditya"
+
+let str3=str1 + str2
+console.log(str3);
+
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
