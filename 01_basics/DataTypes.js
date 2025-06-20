f006947948cdc8c95dada3c6a55d9a5e6ca6832b
Kumaraditya8527 <adityakumar1886@gmail.com>
2025-06-20 14:01:29 +0000

    DataTypes

--- 01_basics/DataTypes.js
+++ 01_basics/DataTypes.js
@@ -13,12 +13,35 @@ reference types
 type:array,function,object ==>dataype is object
 */
 
-const heros=["shaktiman","nagrraj","doga"]
-let myobj={
-    name:"aditya",
-    age:22,   
+// const heros=["shaktiman","nagrraj","doga"]
+// let myobj={
+//     name:"aditya",
+//     age:22,   
+// }
+
+// const myfunc=function(){
+//     console.log("hello world");
+// }
+
+//******************************************************************************************************//
+
+/*stack(primitive) and heap(non-primitive) */
+
+let x="aditya"
+let y=x
+y="kumar"
+console.log(x);
+console.log(y);
+
+let user1={
+    email:"abc@gmail.com",
+    upi:"xyx"
 }
 
-const myfunc=function(){
-    console.log("hello world");
-}
+let user2=user1
+user2.email="aditya@xyz"
+
+console.log(user1.email);
+console.log(user2.email);
+
+
