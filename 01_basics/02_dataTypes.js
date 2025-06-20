132cae2baf54c8407950c51fd5be0207c43957cc
Kumaraditya8527 <adityakumar1886@gmail.com>
2025-06-20 09:32:57 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we are using nodejs not browser
+
+console.log(3+3)
+console.log("Aditya")
+
+let name="aditya"
+let age=3
+let isloggedIN=false
+let state
+
+//number==>2^53
+//Bigint
+//string==>""
+//bbolean==>true/false
+//null==>standalone value
+//undefined
+//symbol==>unique
+
+console.log(typeof(undefined));//undefiend
+console.log(typeof(null));//object
