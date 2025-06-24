8f1228e2390436e3d1e72b86ebd1eeb1ab656075
Kumaraditya8527 <adityakumar1886@gmail.com>
2025-06-24 14:17:25 +0000

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,16 @@
+const myOBject={
+    js:'java script',
+    cpp:'c++',
+    rb:'ruby',
+    swift:'swift by app'
+}
+//for in
+for (const key in myOBject) {
+   console.log(`${key} shorcut is for ${myOBject[key]}`);   
+}
+
+const programming=["js","java","cpp","ruby",'py']
+for (const key in programming) {
+   console.log(programming[key]);
+   
+}
