3df0dea714159c3894940c268d613ef58597a671
Kumaraditya8527 <adityakumar1886@gmail.com>
2025-06-24 09:32:09 +0000

    while and do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,12 @@
+
+let index=0
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index+=2    
+}
+
+let score=1
+do {
+    console.log(`score is ${score}`);
+    score+=1
+} while (score<=5);
