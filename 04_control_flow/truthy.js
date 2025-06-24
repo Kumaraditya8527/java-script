b46ca5f9789da8baec68a3a87963024511b0031c
Kumaraditya8527 <adityakumar1886@gmail.com>
2025-06-24 09:00:04 +0000

    truthy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -21,3 +21,23 @@ const emptyObj={}
 if (Object.keys(emptyObj).length===0) {
     console.log("object is empty");
 }
+
+// Nullish coalescing operator (??): null undefined
+
+let val1;
+// val1=5 ?? 10
+// val1=null ?? 10
+// val1=undefined ?? 15
+val1=null ??10 ?? 15
+
+console.log(val1);
+
+//terniary operator
+
+//condition ? true:false
+
+const price=100
+price>=80?console.log("more than 80"):console.log("less than 80");
+
+
+
